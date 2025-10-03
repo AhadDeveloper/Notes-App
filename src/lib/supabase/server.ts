8be37239f0be215d6ea4cf76a2691982854afb47
AhadDeveloper <ahad75366@gmail.com>
2025-10-03 22:04:46 +0500

    initial commit: next.js and supabase setup

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,21 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabasekey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export async function createSupabaseServerClient() {
+  const cookieStore = await cookies();
+  return createServerClient(supabaseUrl!, supabasekey!, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, options, value }) => {
+          return cookieStore.set(name, value, options);
+        });
+      },
+    },
+  });
+}
